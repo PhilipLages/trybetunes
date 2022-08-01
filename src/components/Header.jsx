cb820cb4a9f04bb0a890714aa35ef6dd594f9473
Philip Lages <psilvaelages@gmail.com>
2022-08-01 19:08:57 -0300

    feat: add requirement 3. Pending last test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
-// import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: '',
+      isLoading: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.handleLoading();
+    const response = await getUser();
+    this.handleLoading();
+    this.setState({ userName: response.name });
+  }
+
+  handleLoading = () => {
+    const { isLoading } = this.state;
+    const verifyLoading = isLoading ? { isLoading: false } : { isLoading: true };
+    this.setState(verifyLoading);
+  }
+
   render() {
+    const { userName, isLoading } = this.state;
     return (
       <div>
-        <header data-testid="header-component">Header</header>
+        <header data-testid="header-component">
+          {isLoading
+            ? <Loading />
+            : (
+              <p>{`Bem vindx, ${userName}`}</p>
+            ) }
+        </header>
       </div>
     );
   }
