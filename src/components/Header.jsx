231e373992a4f7af3c89045e21fbec39d43e1e31
Philip Lages <psilvaelages@gmail.com>
2022-08-01 18:29:53 -0300

    fix: fix style lint problems

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import {getUser} from '../services/userAPI';
+// import { getUser } from '../services/userAPI';
 
 class Header extends Component {
   render() {
