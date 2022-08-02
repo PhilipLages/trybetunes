334ed4622c395cdfcf1275a672b3f290eff8954d
Philip Lages <psilvaelages@gmail.com>
2022-08-02 11:10:03 -0300

    feat: add requirement 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      artistName: '',
+      artistSearch: '',
+      artistSaved: '',
       isDisable: true,
+      isLoading: false,
+      albums: [],
     };
   }
 
+  // async componentDidMount() {
+  //   await this.handleFetchAlbums();
+  // }
+
   handleDisable = () => {
-    const { artistName } = this.state;
+    const { artistSearch } = this.state;
     const TWO = 2;
-    const disable = artistName.length >= TWO ? { isDisable: false } : { isDisable: true };
+    const disable = artistSearch.length >= TWO
+      ? { isDisable: false } : { isDisable: true };
     this.setState(disable);
   }
 
@@ -21,31 +32,75 @@ class Search extends Component {
     this.setState({ [name]: value }, () => this.handleDisable());
   }
 
+  handleLoading = () => {
+    const { isLoading } = this.state;
+    const verifyLoading = isLoading ? { isLoading: false } : { isLoading: true };
+    this.setState(verifyLoading);
+  }
+
+  handleFetchAlbums = async () => {
+    const { artistSearch } = this.state;
+    this.handleLoading();
+    const response = await searchAlbumsAPI(artistSearch);
+    this.handleLoading();
+    this.setState({ albums: response, artistSearch: '', artistSaved: artistSearch });
+  }
+
   render() {
-    const { artistName, isDisable } = this.state;
+    const { artistSearch, artistSaved, isDisable, isLoading, albums } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <label htmlFor="artistName">
-            <p>Pesquisar artistas: </p>
-            <input
-              data-testid="search-artist-input"
-              type="text"
-              value={ artistName }
-              onChange={ this.handleChange }
-              name="artistName"
-              id="artistName"
-            />
-          </label>
-          <button
-            data-testid="search-artist-button"
-            type="button"
-            disabled={ isDisable }
-          >
-            Pesquisar
-          </button>
-        </form>
+        {isLoading
+          ? <Loading />
+          : (
+            <form>
+              <label htmlFor="artistName">
+                <p>Pesquisar álbuns: </p>
+                <input
+                  data-testid="search-artist-input"
+                  type="text"
+                  value={ artistSearch }
+                  onChange={ this.handleChange }
+                  name="artistSearch"
+                  id="artistName"
+                />
+              </label>
+              <button
+                data-testid="search-artist-button"
+                type="button"
+                onClick={ this.handleFetchAlbums }
+                disabled={ isDisable }
+              >
+                Pesquisar
+              </button>
+            </form>
+          )}
+        <section>
+          {albums.length === 0
+            ? (<p>Nenhum álbum foi encontrado</p>)
+            : (
+              <div>
+                <p>{`Resultado de álbuns de: ${artistSaved}`}</p>
+                <div className="albums">
+                  {albums
+                    .map((album) => (
+                      <div key={ album.collectionId }>
+                        <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                        <h2>{album.collectionName}</h2>
+                        <p>{album.artistName}</p>
+                        <Link
+                          to={ `/album/${album.collectionId}` }
+                          data-testid={ `link-to-album-${album.collectionId}` }
+                        >
+                          Ir para o álbum
+                        </Link>
+                      </div>
+                    ))}
+                </div>
+              </div>
+            )}
+        </section>
       </div>
     );
   }
